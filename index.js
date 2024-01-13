38c25218f855fdc1495606139fa98cda0e88440f
Martin Fojtek <martfojt@gmail.com>
2024-01-13 15:05:15 +0100

    Add comments to the code

--- index.js
+++ index.js
@@ -1,13 +1,16 @@
 import { createServer } from 'node:http';
 
+// Set the server port from environment variable or default to 8080
 const PORT = process.env.PORT || 8080;
 
+// Create a new HTTP server
 const server = createServer();
 
 server.on('request', (request, response) => {
   response.end('Hello, world!');
 });
 
+// Start the server and listen on the specified port
 server.listen(PORT, () => {
   console.log(`starting server at port ${PORT}`);
 });
